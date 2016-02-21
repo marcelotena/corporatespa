679cac67e0c337a0385032d893f342a5b44dc304
Marcelo Tena <marcelotena@fastandyours.com>
2016-02-21 13:31:44 +0100

    Added Compass and main style.css now imports a compressed file in the assets/css folder.

--- functions.php
+++ functions.php
@@ -117,7 +117,7 @@ add_action( 'widgets_init', 'corporatespa_widgets_init' );
 function corporatespa_scripts() {
 	wp_enqueue_style( 'corporatespa-style', get_stylesheet_uri() );
 
-	wp_enqueue_script( 'corporatespa-allscripts', get_template_directory_uri() . '/assets/js/min/script.js', array(), '20120206', true );
+	wp_enqueue_script( 'corporatespa-allscripts', get_template_directory_uri() . '/assets/js/min/script.js', array($this, 'jquery'), '20120206', true );
 
 	if ( is_singular() && comments_open() && get_option( 'thread_comments' ) ) {
 		wp_enqueue_script( 'comment-reply' );
--- gulpfile.js
+++ gulpfile.js
@@ -1,18 +1,34 @@
 var gulp = require('gulp'),
     gutil = require('gulp-util'),
     browserify = require('gulp-browserify'),
+    compass = require('gulp-compass'),
     concat = require('gulp-concat');
 
 var jsSources = [
     'assets/js/customizer.js',
     'assets/js/navigation.js',
     'assets/js/skip-link-focus-fix.js'
-]
+];
+
+var sassSources = [
+    'assets/stylesheets/style.scss'
+];
 
 gulp.task('js', function() {
     gulp.src(jsSources)
         .pipe(concat('script.js'))
         .pipe(browserify())
         .pipe(gulp.dest('assets/js/min/'));
-    gutil.log('> Scripts joined');
+    gutil.log('>> Scripts joined <<');
+});
+
+gulp.task('compass', function() {
+    gulp.src(sassSources)
+        .pipe(compass({
+            sass: 'assets/stylesheets',
+            image: 'assets/images',
+            style: 'expanded'
+        }))
+        .pipe(gulp.dest('assets/css/'));
+    gutil.log('>> SASS files processed <<');
 });
